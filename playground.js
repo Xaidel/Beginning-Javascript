7d13747eb63e73caf4d211194181dbb81fc7bf6b
Karl Cydil Dwight Abechuela <abechuelak@gmail.com>
2023-07-11 20:20:23 +0000

    Metacrafters JS Project

--- playground.js
+++ playground.js
@@ -1,15 +1,50 @@
-/* 
-// Metacrafters - Beginning Javascript
-//
-// This is a javascript playground for testing your javascript code!
-// When you are ready to test, simply right click and select "Run Code"
-// to see the result print below. If you have more then one snippet of code,
-// you can highlight the code you want to test, and then right click and select "Run Code"
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
 */
 
-// Enter your code below this line
+// create a variable to hold your NFT's
 
-// Code example
-for(var i=1; i<=5; i++) {
- console.log(i);
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+
+var mintedNFT = [];
+
+function mintNFT (name, description, network) {
+    const NFT={
+        name: name,
+        description: description,
+        network: network
+    };
+
+    mintedNFT.push(NFT);
 }
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+    for(let x = 0; x < mintedNFT.length; x++){
+        NFT = mintedNFT[x];
+        console.log("Name: " + NFT.name + "\n");
+        console.log("Description: " + NFT.description + "\n");
+        console.log("Network: " + NFT.network + "\n");
+    }
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+    console.log(mintedNFT.length);
+}
+
+// call your functions below this line
+mintNFT("SLP", "Test", "Metamask");
+mintNFT("POL", "Test", "Solana");
+listNFTs();
+getTotalSupply();
+ 
